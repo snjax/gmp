069967fd454dcc629d39c28779de431ccb1befeb
Igor Gulamov <igor.gulamov@gmail.com>
2018-11-24 01:28:36 +0300

    fix

--- int.go
+++ int.go
@@ -68,7 +68,7 @@ type Int struct {
 	init bool
 }
 
-func nextPrime(z *Int) *Int {
+func NextPrime(z *Int) *Int {
 	res := new(Int)
 	res.doinit()
 	C.mpz_nextprime((C.mpz_ptr)(unsafe.Pointer(&res.i)), (C.mpz_ptr)(unsafe.Pointer(&z.i)))
